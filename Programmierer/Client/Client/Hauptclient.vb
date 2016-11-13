d9817f7c510b93441049c1ca57929358af1ba9fe
Arayni <Arayni@Day.com>
2016-11-13 17:46:47 +0100

    Delete Demo

--- Programmierer/Client/Client/Hauptclient.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class Hauptclient
-
-End Class
--- Programmierer/Client/Client/Verbinden.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Verbinden
-
-
-    Private Sub btnVerbinden_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnVerbinden.Click
-        Me.Close()
-    End Sub
-End Class
